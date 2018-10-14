dc1ac676c748b6017afca8db6a1f624d63a5761c
Bryn Hill <30795291+bryn-hill@users.noreply.github.com>
2018-10-14 21:01:22 +0200

    feature: add apollo (#69)

--- app/core/components/app.tsx
+++ app/core/components/app.tsx
@@ -5,19 +5,35 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import { Store } from 'redux';
 import { Routes } from './routes';
 import { AppState } from '../reducers/app';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
+export const link = createHttpLink({
+  uri: 'http://localhost/graphql',
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
 export interface AppProps {
   store: Store<AppState>;
-};
+}
 
 export class App extends React.Component<AppProps> {
-  render(): JSX.Element { return (
-    <Provider store={this.props.store}>
-      <BrowserRouter>
-        <Switch>
-          <Routes />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
-  )};
-};
+  render(): JSX.Element {
+    return (
+      <ApolloProvider client={client}>
+        <Provider store={this.props.store}>
+          <BrowserRouter>
+            <Switch>
+              <Routes />
+            </Switch>
+          </BrowserRouter>
+        </Provider>
+      </ApolloProvider>
+    );
+  }
+}
--- app/home/components/home.tsx
+++ app/home/components/home.tsx
@@ -3,18 +3,43 @@ import { Link } from 'react-router-dom';
 import AppBar from '../../Components/Appbar';
 import Card from '../../Components/Card';
 import { Grid } from '@material-ui/core';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const GET_SONGS = gql`
+  query {
+    songs {
+      id
+      title
+      artist
+    }
+    votingResults {
+      votes {
+        votes
+        songId
+      }
+    }
+  }
+`;
 
 export const Home: React.SFC = () => (
   <React.Fragment>
     <AppBar />
     <Grid>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Query query={GET_SONGS}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
+
+          return (
+            <div>
+              {data.songs.map(song => (
+                <Card title={song.title} artist={song.artist} votes={'0'} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
     </Grid>
   </React.Fragment>
 );
